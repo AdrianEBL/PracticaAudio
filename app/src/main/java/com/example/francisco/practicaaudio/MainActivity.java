5e8a37cbb0d43cf7f609388acdd7a234f0a73a85
Adrian <ed.aprio@gmail.com>
2016-11-13 04:03:54 -0700

    Primer commit

--- app/src/main/java/com/example/francisco/practicaaudio/MainActivity.java
+++ app/src/main/java/com/example/francisco/practicaaudio/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
